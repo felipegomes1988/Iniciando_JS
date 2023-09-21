34f43741c8fa321f7d080cea3e3672dafddf5adf
Felipe Behling Gomes <134333946+felipegomes1988@users.noreply.github.com>
2023-09-21 19:39:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+    
+//     💡 Para saber o tipo de dado você pode usar o operador `typeof`
+    
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+//1.
+
+//alert("Hello World!");
+
+//2.
+
+// let firstNumber = prompt("Digite o primeiro número");
+
+// let secondNumber = prompt("DIgite o segundo número");
+
+// alert(Number(firstNumber)+Number(secondNumber));
+
+//3, 4 e 5:
+
+// let input=false;
+
+// console.log(input);
+// console.log(typeof input);
+
+
+// if(typeof input ==="number"){
+//   console.log("It´s a number");
+// }
+// else if(typeof input === "string"){
+//     console.log("It's a string");
+// }
+// else if(typeof input === "boolean"){
+//   console.log("It's a boolean");
+// }
+
+//6.
+
+// let firstNumber = prompt("Digite o primeiro número");
+
+// let secondNumber = prompt("DIgite o segundo número");
+
+// alert(Number(firstNumber)-Number(secondNumber));
+
+//7.
+
+// let firstNumber = prompt("Digite o primeiro número");
+
+// let secondNumber = prompt("DIgite o segundo número");
+
+// alert(Number(firstNumber)*Number(secondNumber));
+
+//8.
+
+// let firstNumber = prompt("Digite o primeiro número");
+
+// let secondNumber = prompt("DIgite o segundo número");
+
+// alert(Number(firstNumber)/Number(secondNumber));
+
+//9 e 10.
+
+// let number = prompt("Digite um número para saber se é par ou ímpar");
+
+// if(number%2==0){
+//   alert("Esse numero é par")
+// }
+// else{
+//   alert("Esse numero é impar")
+// }
+
+
